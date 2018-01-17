e3aeb5601d6ad85496eab24cb761cc21f9a8b5c9
Francisco Jesús Latorre García <piti_fran@hotmail.com>
2018-01-17 23:20:07 +0100

    5. Clase Cliente.

---
+++ src/tarea05/Cliente.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea05;
+
+/**
+ *
+ * @author Francisco Jesus Latorre Garcia <franlatorregarcia@gmail.com>
+ */
+public class Cliente {
+    private String nombre, dni, direccion, localidad, codigoPostal;
+    private int identificador, numClientes;
+}
