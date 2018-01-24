7b94b4a4a06002d437e703d1220bb3b6d88a4689
Francisco Jesús Latorre García <piti_fran@hotmail.com>
2018-01-25 00:11:42 +0100

    14. Constructor con 2 parámetros en la clase Alquiler.

--- src/tarea05/modelo/Alquiler.java
+++ src/tarea05/modelo/Alquiler.java
@@ -8,11 +8,20 @@ import java.text.SimpleDateFormat;
  */
 public class Alquiler {
 
+    private Turismo turismo;
+    private Cliente cliente;
     private Date fecha;
     private int dias;
     private final SimpleDateFormat FORMATO_FECHA = new SimpleDateFormat("dd/MM/yyyy HH:mm");
     private final int MS_DIA = 1000 * 60 * 60 * 24;
     private final double PRECIO_DIA = 10.0;
-    
 
+    public Alquiler(Cliente cliente, Turismo turismo) {
+        this.cliente = cliente;
+        this.turismo = turismo;
+        fecha = new Date();
+        dias = 0;
+        turismo.setDisponible(false);
+    }
+    
 }
