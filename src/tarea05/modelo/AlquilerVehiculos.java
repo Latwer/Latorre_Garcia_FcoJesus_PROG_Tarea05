75fe28bab2427f79d85713223e75fc37ba74eefb
Francisco Jesús Latorre García <piti_fran@hotmail.com>
2018-01-25 01:04:58 +0100

    19. Clase AlquilerVehiculos con sus atributos.

---
+++ src/tarea05/modelo/AlquilerVehiculos.java
@@ -0,0 +1,15 @@
+package tarea05.modelo;
+
+/**
+ * @author Francisco Jesus Latorre Garcia <franlatorregarcia@gmail.com>
+ */
+public class AlquilerVehiculos {
+
+    private Cliente[] clientes;
+    private Turismo[] turismos;
+    private Alquiler[] alquileres;
+
+    private final int MAX_CLIENTES = 25;
+    private final int MAX_TURISMOS = 25;
+    private final int MAX_ALQUILERES = 25;
+}
